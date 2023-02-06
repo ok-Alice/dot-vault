b9e48b8cae7ec8ba3987490fb9f6cc9de36af1b8
zaBuxx <wgo@belgoline.com>
2023-02-06 20:27:53 +0100

    added sol contract

--- evm-contracts/collateral-proxy/contract.sol
+++ evm-contracts/collateral-proxy/contract.sol
@@ -1,2 +1,65 @@
-# empty file
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+interface XVM {
+    function xvm_call(
+        bytes calldata context,
+        bytes calldata to,
+        bytes calldata input
+    ) external;
+}
+
+contract collateral {
+    XVM constant XVM_PRECOMPILE = XVM(0x0000000000000000000000000000000000005005);
+
+    address ink_address;
+
+    constructor (address _ink_address) {
+        ink_address = _ink_address;
+    }
+
+ 
+    function deposit(
+        openbrush_contracts_traits_types_Id  id
+    ) public
+    returns (Result)
+    {
+        bytes4 selector = 0x2d10c9bd;
+        bytes memory contract_address = abi.encodePacked(ink_address);
+        bytes memory buffer = bytes.concat(
+            selector,
+            encode_openbrush_contracts_traits_types_Id(id)
+        );
+
+        XVM_PRECOMPILE.xvm_call("\x1f\x00", contract_address, buffer);
+        return Result.Ok;
+    }
+
+
+
+
+    // mapped id Number(2) -> openbrush_contracts_traits_types_Id
+    enum openbrush_contracts_traits_types_Id {
+        U8, // = 0
+        U16, // = 1
+        U32, // = 2
+        U64, // = 3
+        U128, // = 4
+        Bytes // = 5
+    }
+
+
+    // mapped id Number(8) -> Result
+    enum Result {
+        Ok, // = 0
+        Err // = 1
+    }
+
+
+
+    function encode_uint128(uint128 value) private pure returns (bytes memory) {
+        return abi.encodePacked(value);
+    }
+
+}
 
--- wasm-contracts/collateral/lib.rs
+++ wasm-contracts/collateral/lib.rs
@@ -3,74 +3,54 @@
 
 #[openbrush::contract]
 pub mod psp34_wrapper {
-    use ink_prelude::vec::Vec;
     use ink_storage::traits::SpreadAllocate;
     use openbrush::{
-        contracts::psp34::extensions::metadata::*,
         traits::{
             Storage,
             String,
         },
     };
-    use openbrush::contracts::psp34::{PSP34Error, PSP34Ref};
-    use xvm_sdk_psp34_controller::PSP34ControllerRef;
+    use openbrush::contracts::psp34::{PSP34Error};
     use ethabi::ethereum_types::U256;
     use openbrush::contracts::psp34::Id;
-    use ink_lang::ToAccountId;
     use xvm_helper::XvmErc721;
 
     #[derive(Default, SpreadAllocate, Storage)]
     #[ink(storage)]
     pub struct PSP34Wrapper {
-        #[storage_field]
-        psp34: psp34::Data,
-        #[storage_field]
-        metadata: metadata::Data,
+//        #[storage_field]
         evm_address: [u8; 20],
-        psp34_controller: AccountId,
+        
     }
 
-    impl PSP34 for PSP34Wrapper {}
-
-    impl PSP34Metadata for PSP34Wrapper {}
+    pub enum TmpError {
+        Error,
+    }
 
     impl PSP34Wrapper {
         #[ink(constructor)]
-        pub fn new(version: u32, psp34_controller_hash: Hash, evm_contract_address: [u8; 20], id: Id, name: String, symbol: String) -> Self {
+        pub fn new(evm_contract_address: [u8; 20]) -> Self {
             ink_lang::codegen::initialize_contract(|instance: &mut Self| {
-                let name_key: Vec<u8> = String::from("name");
-                let symbol_key: Vec<u8> = String::from("symbol");
-                instance._set_attribute(id.clone(), name_key, name);
-                instance._set_attribute(id, symbol_key, symbol);
+
                 instance.evm_address = evm_contract_address;
-                let salt = version.to_le_bytes();
-                let psp34 = PSP34ControllerRef::new(evm_contract_address.into())
-                    .endowment(0)
-                    .code_hash(psp34_controller_hash)
-                    .salt_bytes(salt)
-                    .instantiate()
-                    .unwrap_or_else(|error| {
-                        panic!("failed at instantiating the psp34 controller contract: {:?}", error)
-                    });
-                instance.psp34_controller = psp34.to_account_id();
+             
             })
         }
 
         #[ink(message)]
-        pub fn deposit(&mut self, id: Id) -> Result<(), PSP34Error> {
+        pub fn deposit(&mut self, id: Id) -> Result<(),PSP34Error> {
             let caller = self.env().caller();
             let contract = self.env().account_id();
             XvmErc721::transfer_from(self.evm_address, caller, contract, cast(id.clone()))
-                .map_err(|_| PSP34Error::Custom(String::from("transfer failed")))?;
-            self._mint_to(caller, id)
+                .map_err(|_| PSP34Error::Custom(String::from("transfer failed")))
+ 
         }
 
-        #[ink(message)]
-        pub fn withdraw(&mut self, id: Id) -> Result<(), PSP34Error> {
-            let caller = self.env().caller();
-            self._burn_from(caller, id.clone())?;
-            PSP34Ref::transfer(&mut self.psp34_controller, caller, id, Vec::new())
-        }
+        // #[ink(message)]
+        // pub fn withdraw(&mut self, id: Id) -> Result<(),PSP34Error> {
+        //     let caller = self.env().caller();
+        //     PSP34Ref::transfer(&mut self.psp34_controller, caller, id, Vec::new())
+        // }
     }
 
     fn cast(id: Id) -> U256 {
