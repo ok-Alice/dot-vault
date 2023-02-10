1ce560663755af90d560daeb3ab1070c97ff21b7
zaBuxx <wgo@belgoline.com>
2023-02-10 20:10:15 +0100

    initial

---
+++ wasm-contracts/signtransfer/lib.rs
@@ -0,0 +1,78 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+/// EVM ID (from astar runtime)
+const EVM_ID: u8 = 0x0F;
+
+#[ink::contract(env = xvm_environment::XvmDefaultEnvironment)]
+mod signtransfer {
+    use ethabi::{
+        ethereum_types::{
+            H160,
+            U256,
+        },
+        Token,
+    };
+    use hex_literal::hex;
+    use ink_prelude::{
+        string::{
+            String,
+        },
+        vec::Vec,
+    };
+
+    const TRANSFER_FROM_SELECTOR: [u8; 4] = hex!["23b872dd"];
+
+    /// Only one Error is supported
+    #[derive(Debug, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum STError {
+        Custom(String),
+    } 
+
+    #[ink(storage)]
+    pub struct ST {
+       // evm_address: [u8; 20],
+    }
+
+
+    impl ST {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {  }
+        }
+
+        #[ink(message, selector = 0x3128d61b)]
+        pub fn transfer(&mut self, evm_address: [u8; 20], to: AccountId, id: u32, _data: Vec<u8>) -> Result<(), STError> {
+            let caller = self.env().caller();
+            let encoded_input = Self::transfer_from_encode(Self::h160(&caller), Self::h160(&to), id.into());
+            self.env()
+                .extension()
+                .xvm_call(
+                    super::EVM_ID,
+                    Vec::from(evm_address.as_ref()),
+                    encoded_input,
+                )
+                .map_err(|_| STError::Custom(String::from("transfer failed")))
+        }
+
+        fn transfer_from_encode(from: H160, to: H160, token_id: U256) -> Vec<u8> {
+            let mut encoded = TRANSFER_FROM_SELECTOR.to_vec();
+            let input = [
+                Token::Address(from),
+                Token::Address(to),
+                Token::Uint(token_id),
+            ];
+            encoded.extend(&ethabi::encode(&input));
+            encoded
+        }
+
+        fn h160(from: &AccountId) -> H160 {
+            let mut dest: H160 = [0; 20].into();
+            dest.as_bytes_mut()
+                .copy_from_slice(&<ink_env::AccountId as AsRef<[u8]>>::as_ref(from)[..20]);
+            dest
+        }
+    }
+}
